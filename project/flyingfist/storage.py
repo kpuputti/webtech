e0eef64fd90e7847da68f6bd5459dd817d336069
Kimmo Puputti <kimmo.puputti@hexagonit.fi>
2010-05-14 19:27:41 +0200

    Created outline for the Storage class.

---
+++ project/flyingfist/storage.py
@@ -0,0 +1,48 @@
+"""Helper functions to create the RDF storage.
+"""
+from flyingfist import settings
+
+
+class Storage(object):
+
+    def __init__(self):
+        pass
+
+    def _add_admin1_codes(self):
+        pass
+
+    def _add_admin2_codes(self):
+        pass
+
+    def _add_feature_codes(self):
+        pass
+
+    def _add_country_info_columns(self):
+        pass
+
+    def _add_feature_columns(self):
+        pass
+
+    def _create_ontologies(self):
+        """Create the ontology structure.
+
+        Data is read from the Geonames files and an RDF ontology is
+        created of the classes and properties.
+        """
+        self._add_admin1_codes()
+        self._add_admin2_codes()
+        self._add_feature_codes()
+        self._add_country_info_columns()
+        self._add_feature_columns()
+
+    def _add_data(self):
+        """Add all the instance data to the RDF storage."""
+
+    def _clear(self):
+        """Clears the whole RDF storage."""
+
+    def create(self):
+        """Create the RDF storage structure and add the instance data."""
+        self._clear()
+        self._create_ontologies()
+        self._add_data()
