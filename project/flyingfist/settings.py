ac3c4e3632437b060b04dcdc14f772cec1e27eed
Kimmo Puputti <kimmo.puputti@hexagonit.fi>
2010-05-14 18:12:33 +0200

    Added feature column names.

--- project/flyingfist/settings.py
+++ project/flyingfist/settings.py
@@ -47,3 +47,47 @@ COLUMNS_COUNTRY_INFO = (
     'neighbours',
     'EquivalentFipsCode',
 )
+
+# NL.txt column names:
+#
+#  geonameid         : integer id of record in geonames database
+#  name              : name of geographical point (utf8) varchar(200)
+#  asciiname         : name of geographical point in plain ascii characters, varchar(200)
+#  alternatenames    : alternatenames, comma separated varchar(5000)
+#  latitude          : latitude in decimal degrees (wgs84)
+#  longitude         : longitude in decimal degrees (wgs84)
+#  feature class     : see http://www.geonames.org/export/codes.html, char(1)
+#  feature code      : see http://www.geonames.org/export/codes.html, varchar(10)
+#  country code      : ISO-3166 2-letter country code, 2 characters
+#  cc2               : alternate country codes, comma separated, ISO-3166 2-letter country code, 60 characters
+#  admin1 code       : fipscode (subject to change to iso code), see exceptions below, see file admin1Codes.txt for display names of this code; varchar(20)
+#  admin2 code       : code for the second administrative division, a county in the US, see file admin2Codes.txt; varchar(80)
+#  admin3 code       : code for third level administrative division, varchar(20)
+#  admin4 code       : code for fourth level administrative division, varchar(20)
+#  population        : bigint (8 byte int)
+#  elevation         : in meters, integer
+#  gtopo30           : average elevation of 30'x30' (ca 900mx900m) area in meters, integer
+#  timezone          : the timezone id (see file timeZone.txt)
+#  modification date : date of last modification in yyyy-MM-dd format
+
+COLUMNS_FEATURE = (
+    'geonameid',
+    'name',
+    'asciiname',
+    'alternatenames',
+    'latitude',
+    'longitude',
+    'feature class',
+    'feature code',
+    'country code',
+    'cc2',
+    'admin1 code',
+    'admin2 code',
+    'admin3 code',
+    'admin4 code',
+    'population',
+    'elevation',
+    'gtopo30',
+    'timezone',
+    'modification date',
+)
