39dc94c90001827bbf93517400ac9752d88eaf0a
Kimmo Puputti <kimmo.puputti@hexagonit.fi>
2010-05-14 18:00:53 +0200

    Added namespaces to settings.

---
+++ project/flyingfist/settings.py
@@ -0,0 +1,9 @@
+# Namespace definitions.
+_NS_DOMAIN = 'http://example.org/'
+_NS_BASE = _NS_DOMAIN + 'flyingfist/'
+
+NS_PROPERTIES = _NS_BASE + 'properties/'
+NS_FEATURES = _NS_BASE + 'features/'
+
+NS_ADMIN1_CODES = _NS_BASE + 'admin1Codes/'
+NS_ADMIN2_CODES = _NS_BASE + 'admin2Codes/'
