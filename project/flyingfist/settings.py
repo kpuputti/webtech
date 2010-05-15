0af41414a9c340da717738e7bbfda4e68590821c
Kimmo Puputti <kimmo.puputti@hexagonit.fi>
2010-05-15 15:29:07 +0200

    Combined admin 1 and 2 code namespaces.

--- project/flyingfist/settings.py
+++ project/flyingfist/settings.py
@@ -17,8 +17,8 @@ _NS_BASE = _NS_DOMAIN + 'flyingfist/'
 NS_PROPERTIES = _NS_BASE + 'properties/'
 NS_FEATURES = _NS_BASE + 'features/'
 
-NS_ADMIN1_CODES = _NS_BASE + 'admin1Codes/'
-NS_ADMIN2_CODES = _NS_BASE + 'admin2Codes/'
+NS_CLASSES = _NS_BASE + 'classes/'
+NS_ADMIN_CODES = _NS_BASE + 'adminCodes/'
 
 # countryInfo.txt column names:
 #
