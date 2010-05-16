aed503bc2a222ba7abf061a7601512787b5ae9e8
Kimmo Puputti <kimmo.puputti@hexagonit.fi>
2010-05-16 21:03:28 +0200

    Added SPARQL test to check feature labels.

--- project/tests/test_ontology.py
+++ project/tests/test_ontology.py
@@ -10,7 +10,18 @@ ontology = graph.ConjunctiveGraph()
 ontology.parse(settings.ONTOLOGY_FILE, format='n3')
 #ontology.parse(settings.INSTANCES_FILE, format='n3')
 
+FEATURE_CODE_COUNT = 671
 FF = Namespace(settings.NS_FLYINGFIST)
+
+SPARQL_BASE = """\
+PREFIX rdf:<%s>
+PREFIX rdfs:<%s>
+PREFIX ff:<%s>
+
+SELECT ?res
+WHERE {
+
+""" % (str(RDF), str(RDFS), str(FF))
 
 class OntologyTest(unittest.TestCase):
 
@@ -19,4 +30,13 @@ class OntologyTest(unittest.TestCase):
 
     def test_feature_code_count(self):
         feature_codes = ontology.triples((None, RDFS.subClassOf, FF['Feature']))
-        self.assertEquals(len(list(feature_codes)), 671)
+        self.assertEquals(len(list(feature_codes)), FEATURE_CODE_COUNT)
+
+    def test_feature_labels(self):
+        query = SPARQL_BASE + """\
+        ?res rdfs:subClassOf ff:Feature .
+        ?res rdfs:label ?l
+        }
+        """
+        result = ontology.query(query)
+        self.assertEquals(len(result), FEATURE_CODE_COUNT)
