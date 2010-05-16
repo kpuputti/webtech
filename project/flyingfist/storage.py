5fca1ca188645b7e964807d217ff380703625742
Kimmo Puputti <kimmo.puputti@hexagonit.fi>
2010-05-16 13:57:43 +0200

    Separate ontology from the instances.

--- project/flyingfist/storage.py
+++ project/flyingfist/storage.py
@@ -15,18 +15,29 @@ logger = logging.getLogger('flyingfist.storage')
 class StorageCreator(object):
 
     def __init__(self):
-        self.graph = graph.ConjunctiveGraph()
+        self.ontology = graph.ConjunctiveGraph()
+        self.instances = graph.ConjunctiveGraph()
         self.flyingfist = Namespace(settings.NS_FLYINGFIST)
-        self.graph.bind('flyingfist', self.flyingfist)
+        self.ontology.bind('flyingfist', self.flyingfist)
+        self.instances.bind('flyingfist', self.flyingfist)
+
+    def _add_classes(self):
+        admin_code_class = self.flyingfist['Admin1Code']
+        self.ontology.add((admin_code_class, RDF.type, RDFS.Class))
+        self.ontology.add((admin_code_class, RDFS.label, Literal('admin 1 code')))
+        admin_code_class = self.flyingfist['Admin2Code']
+        self.ontology.add((admin_code_class, RDF.type, RDFS.Class))
+        self.ontology.add((admin_code_class, RDFS.label, Literal('admin 2 code')))
+
+
+    def _add_properties(self):
+        feature = self.flyingfist['feature']
+        self.ontology.add((feature, RDF.type, RDF.Property))
+        self.ontology.add((feature, RDFS.label, Literal('feature')))
 
     def _add_admin1_codes(self):
         logger.info('Adding admin 1 codes to the graph.')
-
-        # Add the admin1Code Class to the graph.
-        admin_code_class = self.flyingfist['admin1Code']
-        self.graph.add((admin_code_class, RDF.type, RDFS.Class))
-        self.graph.add((admin_code_class, RDFS.label, Literal('admin 1 code')))
-
+        admin_code_class = self.flyingfist['Admin1Code']
         with open(settings.FILE_ADMIN1_CODES) as f:
             for line in f:
                 line = line.decode('utf-8').strip()
@@ -35,35 +46,27 @@ class StorageCreator(object):
                 except ValueError:
                     code, name = line, None
                 code_instance = self.flyingfist[code]
-                self.graph.add((code_instance, RDF.type,
-                                admin_code_class))
+                self.instances.add((code_instance, RDF.type,
+                                    admin_code_class))
                 if name is not None:
-                    self.graph.add((code_instance, RDFS.label,
-                                    Literal(name)))
+                    self.instances.add((code_instance, RDFS.label,
+                                        Literal(name)))
 
     def _add_admin2_codes(self):
         logger.info('Adding admin 2 codes to the graph.')
-
-        # Add the admin2Code Class to the graph.
-        admin_code_class = self.flyingfist['admin2Code']
-        self.graph.add((admin_code_class, RDF.type, RDFS.Class))
-        self.graph.add((admin_code_class, RDFS.label, Literal('admin 2 code')))
-
+        admin_code_class = self.flyingfist['Admin2Code']
         feature = self.flyingfist['feature']
-        self.graph.add((feature, RDF.type, RDF.Property))
-        self.graph.add((feature, RDFS.label, Literal('feature')))
-
         with open(settings.FILE_ADMIN2_CODES) as f:
             for line in f:
                 line = line.decode('utf-8').strip()
                 code, name, asciiname, geonameid = line.split('\t')
                 code_instance = self.flyingfist[code]
-                self.graph.add((code_instance, RDF.type,
-                                admin_code_class))
-                self.graph.add((code_instance, RDFS.label,
-                                Literal(name)))
-                self.graph.add((code_instance, feature,
-                                self.flyingfist[geonameid]))
+                self.instances.add((code_instance, RDF.type,
+                                    admin_code_class))
+                self.instances.add((code_instance, RDFS.label,
+                                    Literal(name)))
+                self.instances.add((code_instance, feature,
+                                    self.flyingfist[geonameid]))
 
     def _add_feature_codes(self):
         pass
@@ -80,6 +83,8 @@ class StorageCreator(object):
         Data is read from the Geonames files and an RDF ontology is
         created of the classes and properties.
         """
+        self._add_classes()
+        self._add_properties()
         self._add_admin1_codes()
         self._add_admin2_codes()
         self._add_feature_codes()
@@ -98,12 +103,16 @@ class StorageCreator(object):
         self._create_ontologies()
         self._add_data()
 
-    def save(self, file_name):
+    def save(self, ontology_name, instances_name):
         """Serialize the graph into files."""
-        logger.info('Saving serialized graph into file: %s' % file_name)
-        with open(file_name, 'w') as f:
-            f.write(self.graph.serialize(format='n3'))
-        logger.info('Graph saved successfully.')
+        logger.info('Saving serialized ontology into file: %s' % ontology_name)
+        with open(ontology_name, 'w') as f:
+            f.write(self.ontology.serialize(format='n3'))
+        logger.info('Ontology saved successfully.')
+        logger.info('Saving serialized instances into file: %s' % instances_name)
+        with open(instances_name, 'w') as f:
+            f.write(self.instances.serialize(format='n3'))
+        logger.info('Instances saved successfully.')
 
 
 class Storage(object):
