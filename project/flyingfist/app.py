bd165106f4fa7e65bd4f384ee6b1676be4d2c7cd
Kimmo Puputti <kimmo.puputti@hexagonit.fi>
2010-06-05 20:24:26 +0200

    Made a simple search page that connects to the Lucene index.

--- project/flyingfist/app.py
+++ project/flyingfist/app.py
@@ -1,4 +1,6 @@
 from flyingfist import settings
+from flyingfist import storage
+from flyingfist import utils
 from mako import lookup
 from rdflib import Literal
 from rdflib import Namespace
@@ -7,26 +9,42 @@ from rdflib import graph
 import cherrypy
 import itertools
 import logging
+import lucene
 
 
 logger = logging.getLogger('flyingfist.app')
 tmpl_lookup = lookup.TemplateLookup(directories=[settings.TEMPLATE_FOLDER],
                                     input_encoding='utf-8')
 FF = Namespace(settings.NS_FLYINGFIST)
-
+lucene.initVM()
 
 class FlyingFist(object):
 
     def __init__(self):
         logger.info('Reading in the ontology, might take a while...')
         self.ontology = graph.ConjunctiveGraph()
-        self.ontology.parse(settings.ONTOLOGY_FILE, format='n3')
-        self.ontology.parse(settings.INSTANCES_FILE, format='n3')
+        #self.ontology.parse(settings.ONTOLOGY_FILE, format='n3')
+        #self.ontology.parse(settings.INSTANCES_FILE, format='n3')
         logger.info('Finished reading the ontology.')
+        self.storage = storage.Storage()
 
     @cherrypy.expose
     def index(self):
-        return 'goto <a href="/flyingfist">/flyingfist</a>'
+        return 'index'
+
+    @cherrypy.expose
+    def search(self, q=None):
+        lucene.getVMEnv().attachCurrentThread()
+        if q is None:
+            query = ''
+            hits = 0
+        else:
+            query = utils.escape_html(q)
+            hits, places = self.storage.search(q)
+        return tmpl_lookup.get_template('search.mako').render_unicode(
+            query=query,
+            hits=hits,
+            places=places)
 
     def get_label(self, uri):
         triples = list(self.ontology.triples((uri, RDFS.label, None)))
