b5d269766a682ef4ee1bf15985c77ca523546ac0
Kimmo Puputti <kimmo.puputti@hexagonit.fi>
2010-06-06 19:15:10 +0200

    Added a map to the feature page and removed old cruft.

--- project/static/js/scripts.js
+++ project/static/js/scripts.js
@@ -1,5 +1,5 @@
 /*jslint white: true, browser: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true */
-/*global jquery: false, $: false, google: false, console: false*/
+/*global jquery: false, $: false, google: false, console: false, PLACE: false*/
 
 var LOG = function (s) {
     try {
@@ -10,9 +10,52 @@ var LOG = function (s) {
 // Root namespace for all code.
 var FF = {};
 
+FF.feature = (function () {
+
+    var map,
+        point,
+        marker,
+
+        initMap = function (mapContainer) {
+            try {
+                point = new google.maps.LatLng(
+                    parseFloat(PLACE.latitude, 10),
+                    parseFloat(PLACE.longitude, 10)
+                );
+            } catch (e) {
+                LOG('Place coordinates invalid.');
+                return;
+            }
+            LOG('Initializing map.');
+            LOG(point);
+            map = new google.maps.Map(mapContainer, {
+                center: point,
+                zoom: 13,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
+            marker = new google.maps.Marker({
+                position: point,
+                title: PLACE.label,
+                map: map
+            });
+        };
+
+    return {
+        init: function () {
+            var mapElem = $('#map');
+            if (mapElem.length !== 1 || !PLACE) {
+                LOG('Map elem not found, not initializing map.');
+                return;
+            }
+            initMap(mapElem.get(0));
+        }
+    };
+}());
+
+
 $(document).ready(function () {
-    LOG('init page');
     $('#q').focus().autocomplete({
         source: '/api?method=autocomplete'
     });
+    FF.feature.init();
 });
--- project/templates/feature.mako
+++ project/templates/feature.mako
@@ -2,62 +2,10 @@
 
 <%def name="title()">${label}</%def>
 
-<h1>${label} (${uri})</h1>
+<h1>${label}</h1>
 
-<h2>As a subject:</h2>
+<div id="map"></div>
 
-<ul>
-  % for subject, predicate, tobject in subject_data:
-  <li>
-
-    <strong>${label}</strong>
-
-    -
-
-    % if predicate['local']:
-      <a href="${predicate['uri']}">${predicate['label']}</a>
-    % else:
-      ${predicate['label']}
-    % endif
-
-    -
-
-    % if tobject.get('text', False):
-      ${tobject['text']}
-    % elif tobject['local']:
-      <a href="${tobject['uri']}">${tobject['label']}</a>
-    % else:
-      ${tobject['label']}
-    % endif
-
-  </li>
-  % endfor
-</ul>
-
-<h2>As an object:</h2>
-
-<ul>
-  % for subject, predicate, tobject in object_data:
-  <li>
-
-    % if subject['local']:
-      <a href="${subject['uri']}">${subject['label']}</a>
-    % else:
-      ${subject['uri']}
-    % endif
-
-    -
-
-    % if predicate['local']:
-      <a href="${predicate['uri']}">${predicate['label']}</a>
-    % else:
-      ${predicate['label']}
-    % endif
-
-    -
-
-    <strong>${label}</strong>
-
-  </li>
-  % endfor
-</ul>
+<script type="text/javascript">
+  var PLACE = ${info};
+</script>
