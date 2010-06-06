dd9504bce0245b1444c3255af20f0ea98e386bca
Kimmo Puputti <kimmo.puputti@hexagonit.fi>
2010-06-06 19:14:00 +0200

    Removed unused functions.

--- project/flyingfist/app.py
+++ project/flyingfist/app.py
@@ -54,69 +54,21 @@ class FlyingFist(object):
             hits=hits,
             places=places)
 
-    def get_label(self, uri):
-        triples = list(self.ontology.triples((uri, RDFS.label, None)))
-        if triples:
-            return triples[0][2]
-        return uri
-
-    def get_data(self, uri, label):
-        subject_triples = self.ontology.triples((uri, None, None))
-        object_triples = self.ontology.triples((None, None, uri))
-        subject_data = []
-        object_data = []
-
-        for triple in itertools.chain(subject_triples, object_triples):
-
-            subject = triple[0]
-            predicate = triple[1]
-            tobject = triple[2]
-
-            item = [
-                {
-                    'uri': str(subject),
-                    'label': self.get_label(subject),
-                    'local': subject.startswith(FF),
-                },
-                {
-                    'uri': str(predicate),
-                    'label': self.get_label(predicate),
-                    'local': subject.startswith(FF),
-                },
-                {
-                    'local': tobject.startswith(FF),
-                },
-            ]
-
-            if type(tobject) == Literal:
-                item[2]['text'] = tobject
-            else:
-                item[2]['uri'] = str(tobject)
-                item[2]['label'] = self.get_label(tobject)
-
-            if str(subject) == str(uri):
-                subject_data.append(item)
-            else:
-                object_data.append(item)
-        return subject_data, object_data
-
     @cherrypy.expose
     def flyingfist(self, param=None):
         if param is None:
             return tmpl_lookup.get_template('index.mako').render()
 
         uri = FF[param]
-        label = self.get_label(uri) or ''
-        subject_data, object_data = self.get_data(uri, label)
+        label = self.storage.get_label(self.ontology, uri) or ''
 
-        if not subject_data and not object_data:
-            raise cherrypy.HTTPError(status=404, message='resource not found')
+        info_str = json.dumps(self.storage.place_info_simple(self.ontology, param))
 
         return tmpl_lookup.get_template('feature.mako').render_unicode(
             label=label,
             uri=uri,
-            subject_data=subject_data,
-            object_data=object_data)
+            info=info_str)
+
 
     @cherrypy.expose
     def api(self, method=None, term=None):
