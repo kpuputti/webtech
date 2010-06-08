793b135fb909c6e5a567b1b7c452475fed6b1955
Kimmo Puputti <kimmo.puputti@hexagonit.fi>
2010-06-08 02:14:05 +0200

    Added a function for creating a sparql query.

--- project/flyingfist/utils.py
+++ project/flyingfist/utils.py
@@ -1,3 +1,12 @@
+from flyingfist import settings
+from rdflib import Namespace
+from rdflib import RDF
+from rdflib import RDFS
+
+
+FF = Namespace(settings.NS_FLYINGFIST)
+
+
 def escape_html(s):
     escapes = (
         ('&', '&amp;'),
@@ -10,3 +19,20 @@ def escape_html(s):
     for ch, seq in escapes:
         escaped = escaped.replace(ch, seq)
     return escaped
+
+
+def get_sparql_query(select='?res', body=''):
+    return '''\
+    PREFIX rdf:<%(rdf)s>
+    PREFIX rdfs:<%(rdfs)s>
+    PREFIX ff:<%(ff)s>
+    SELECT %(select)s
+    WHERE {
+    %(body)s
+    }''' % ({
+        'rdf': str(RDF),
+        'rdfs': str(RDFS),
+        'ff': str(FF),
+        'select': select,
+        'body': body,
+    })
